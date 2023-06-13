fbc811c51e40c99e5e51b9e88273d76247cb4380
Naunn <bartosz.m.lewandowski@gmail.com>
2023-06-13 21:50:21 +0200

    typo

--- prediction_methods/lasso_regression.R
+++ prediction_methods/lasso_regression.R
@@ -1,4 +1,4 @@
-# RIDGE REGRESSION =====================================================================================================
+# LASSO REGRESSION =====================================================================================================
 # In statistics and machine learning, lasso (least absolute shrinkage and selection operator; also Lasso or LASSO) is a
 # regression analysis method that performs both variable selection and regularization in order to enhance the prediction 
 # accuracy and interpretability of the resulting statistical model. It was originally introduced in geophysics, and 
