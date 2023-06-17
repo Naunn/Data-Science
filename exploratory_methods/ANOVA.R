06b1106c8b20ce79d26fc6b265656b44f0db2e3d
Naunn <bartosz.m.lewandowski@gmail.com>
2023-06-17 20:42:42 +0200

    ANOVA intro

---
+++ exploratory_methods/ANOVA.R
@@ -0,0 +1,17 @@
+# ANOVA ================================================================================================================
+# Analysis of variance (ANOVA) is a collection of statistical models and their associated estimation procedures 
+# (such as the "variation" among and between groups) used to analyze the differences among means. ANOVA was developed
+# by the statistician Ronald Fisher. ANOVA is based on the law of total variance, where the observed variance in a 
+# particular variable is partitioned into components attributable to different sources of variation. In its simplest 
+# form, ANOVA provides a statistical test of whether two or more population means are equal, and therefore generalizes 
+# the t-test beyond two means. In other words, the ANOVA is used to test the difference between two or more means.
+# ANOVA attempt to express one dependent variable as a linear combination of other features or measurements, also
+# it uses categorical independent variables and a continuous dependent variable,
+
+# Libraries ============================================================================================================
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(caret)
+
+# Data =================================================================================================================
